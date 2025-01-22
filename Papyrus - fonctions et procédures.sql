2d46604fe77179cae228374888cc7d040b8933e7
LucasK <lucas.kuntz@afpa-dev-pompey.fr>
2025-01-22 14:10:54 +0100

    Fini triggers

--- Papyrus - fonctions et procédures.sql
+++ Papyrus - fonctions et procédures.sql
@@ -1,24 +1,445 @@
-use [Papyrus]
+USE [Papyrus]
 GO
 
-create function fn_Dateformat
-(@pdate datetime, @psep char(1))
-returns char(10)
-as
-begin
-	-- variables nécessaires
-	declare @jj char(2);
-	declare @mm char(2);
-	declare @aa char(4);
-	-- calcul des différentes parties de la date
-	set @jj = convert(varchar(2), datepart(dd, @pdate));
-	print @jj;
-	print datepart(dd, @pdate);
-	print @pdate;
-	set @mm = convert(varchar(2), datepart(mm, @pdate));
-	set @jj = convert(varchar(4), datepart(yy, @pdate));
-	-- assemblage pour restitution
-	return @jj + @psep + @mm + @psep + @aa;
-end
+---------------------
+-- 2.1
+---------------------
+SELECT NUMCOM,
+	DATECOM
+FROM [Papyrus].[dbo].[COMMANDE]
+WHERE MONTH(DATECOM) IN (
+		3,
+		4
+		)
+GO
 
-drop function dbo.fn_Dateformat
+DROP FUNCTION [dbo].[fn_Dateformat]
+GO
+
+CREATE FUNCTION fn_Dateformat (
+	@pdate DATETIME,
+	@psep CHAR(1)
+	)
+RETURNS CHAR(10)
+AS
+BEGIN
+	-- variables nécessaires
+	DECLARE @jj CHAR(2);
+	DECLARE @mm CHAR(2);
+	DECLARE @aa CHAR(4);
+
+	-- calcul des différentes parties de la date
+	SET @jj = RIGHT('00' + convert(VARCHAR(2), datepart(dd, @pdate)), 2);
+	SET @mm = RIGHT('00' + convert(VARCHAR(2), datepart(mm, @pdate)), 2);
+	SET @aa = convert(VARCHAR(4), datepart(yy, @pdate));
+
+	-- assemblage pour restitution
+	RETURN @jj + @psep + @mm + @psep + @aa;
+END
+GO
+
+SELECT NUMCOM AS 'Commande',
+	[dbo].[fn_Dateformat](DATECOM, '/') AS 'Date Commande'
+FROM [Papyrus].[dbo].[COMMANDE]
+WHERE MONTH(DATECOM) IN (
+		3,
+		4
+		)
+GO
+
+---------------------
+-- 2.2
+---------------------
+SELECT NUMFOU,
+	NOMFOU,
+	SATISF
+FROM [Papyrus].[dbo].[FOURNISSEUR]
+GO
+
+DROP FUNCTION [dbo].[fn_Satisfaction]
+GO
+
+CREATE FUNCTION fn_Satisfaction (@psatisf INT)
+RETURNS VARCHAR(20)
+AS
+BEGIN
+	DECLARE @labelSatisf VARCHAR(20)
+
+	SET @labelSatisf = 'sans commentaire';
+
+	IF (
+			@psatisf = 1
+			OR @psatisf = 2
+			)
+	BEGIN
+		SET @labelSatisf = 'Mauvais';
+	END
+	ELSE IF (
+			@psatisf = 3
+			OR @psatisf = 4
+			)
+	BEGIN
+		SET @labelSatisf = 'Passable';
+	END
+	ELSE IF (
+			@psatisf = 5
+			OR @psatisf = 6
+			)
+	BEGIN
+		SET @labelSatisf = 'Moyen';
+	END
+	ELSE IF (
+			@psatisf = 7
+			OR @psatisf = 8
+			)
+	BEGIN
+		SET @labelSatisf = 'Bon';
+	END
+	ELSE IF (
+			@psatisf = 9
+			OR @psatisf = 10
+			)
+	BEGIN
+		SET @labelSatisf = 'Excellent';
+	END
+
+	RETURN @labelSatisf;
+END
+GO
+
+SELECT NUMFOU AS 'N° Fournisseur',
+	NOMFOU AS 'Fournisseur',
+	[dbo].[fn_Satisfaction](SATISF) AS 'Satisfaction'
+FROM [Papyrus].[dbo].[FOURNISSEUR]
+ORDER BY SATISF DESC
+GO
+
+---------------------
+-- 2.3
+---------------------
+SELECT FOURNISSEUR.NUMFOU,
+	NOMFOU,
+	SUM(QTECDE * PRIUNI * 1.2) AS CA,
+	YEAR(DATECOM) AS 'Année'
+FROM [Papyrus].[dbo].[FOURNISSEUR]
+INNER JOIN [Papyrus].[dbo].[COMMANDE]
+	ON FOURNISSEUR.NUMFOU = COMMANDE.NUMFOU
+INNER JOIN [Papyrus].[dbo].[LIGNE]
+	ON COMMANDE.NUMCOM = LIGNE.NUMCOM
+--WHERE YEAR(DATECOM) = 2018
+GROUP BY FOURNISSEUR.NUMFOU,
+	NOMFOU,
+	YEAR(DATECOM)
+GO
+
+DROP FUNCTION [dbo].[fn_CA_Fournisseur]
+GO
+
+CREATE FUNCTION fn_CA_Fournisseur (
+	@pfourn INT,
+	@pannee INT
+	)
+RETURNS TABLE
+AS
+RETURN (
+		SELECT FOURNISSEUR.NUMFOU,
+			NOMFOU,
+			FORMAT(SUM(QTECDE * PRIUNI * 1.2), 'C', 'fr-fr') AS CA
+		FROM [Papyrus].[dbo].[FOURNISSEUR]
+		INNER JOIN [Papyrus].[dbo].[COMMANDE]
+			ON FOURNISSEUR.NUMFOU = COMMANDE.NUMFOU
+		INNER JOIN [Papyrus].[dbo].[LIGNE]
+			ON COMMANDE.NUMCOM = LIGNE.NUMCOM
+		WHERE YEAR(DATECOM) = @pannee
+			AND FOURNISSEUR.NUMFOU = @pfourn
+		GROUP BY FOURNISSEUR.NUMFOU,
+			NOMFOU
+		)
+GO
+
+SELECT *
+FROM [dbo].[fn_CA_Fournisseur](1801, 2018)
+GO
+
+---------------------
+-- 2.3.1
+---------------------
+DROP FUNCTION [dbo].[fn_Compte]
+GO
+
+DROP TABLE [dbo].[FOURNISSEUR_IND]
+GO
+
+CREATE TABLE FOURNISSEUR_IND (
+	NUMFOU INT NOT NULL,
+	NOMFOU VARCHAR(35) NULL,
+	RUEFOU VARCHAR(35) NULL,
+	POSFOU CHAR(5) NULL,
+	VILFOU VARCHAR(30) NULL,
+	CONFOU VARCHAR(15) NULL,
+	SATISF INT NULL,
+	CONSTRAINT PK_FOURNISSEUR_IND PRIMARY KEY (NUMFOU)
+	)
+GO
+
+INSERT [dbo].[FOURNISSEUR_IND] (
+	[NUMFOU],
+	[NOMFOU],
+	[RUEFOU],
+	[POSFOU],
+	[VILFOU],
+	[CONFOU],
+	[SATISF]
+	)
+VALUES (
+	1701,
+	N'Les Gros Bits',
+	N'1 Rue du Port',
+	N'38140',
+	N'Rives',
+	N'Michel',
+	8
+	)
+
+INSERT [dbo].[FOURNISSEUR_IND] (
+	[NUMFOU],
+	[NOMFOU],
+	[RUEFOU],
+	[POSFOU],
+	[VILFOU],
+	[CONFOU],
+	[SATISF]
+	)
+VALUES (
+	1706,
+	N'Internet.com',
+	N'8 rue Principale',
+	N'77940',
+	N'Schnafon',
+	N'Simon',
+	7
+	)
+GO
+
+CREATE FUNCTION [dbo].[fn_Compte] ()
+RETURNS INT
+	WITH SCHEMABINDING
+AS
+BEGIN
+	DECLARE @rowsCount INT
+
+	SET @rowsCount = (
+			SELECT COUNT(*)
+			FROM [dbo].[FOURNISSEUR_IND]
+			)
+
+	RETURN @rowsCount;
+END
+GO
+
+---------------------
+-- 3.1
+---------------------
+DROP PROCEDURE [dbo].[prc_Lst_fournis]
+GO
+
+CREATE PROCEDURE [dbo].[prc_Lst_fournis]
+AS
+BEGIN
+	SELECT DISTINCT NUMFOU
+	FROM dbo.COMMANDE
+END
+GO
+
+EXEC prc_Lst_fournis
+GO
+
+sp_help prc_Lst_fournis
+	-- Name / Owner / Type / Created_datetime
+GO
+
+sp_helptext prc_Lst_fournis
+	-- Body of procedure
+GO
+
+---------------------
+-- 3.2
+---------------------
+DROP PROCEDURE [dbo].[prc_Lst_Commandes]
+GO
+
+CREATE PROCEDURE [dbo].[prc_Lst_Commandes] @vobs VARCHAR(35)
+AS
+BEGIN
+	SELECT COMMANDE.NUMCOM AS NuméroCommande,
+		NOMFOU,
+		LIBART,
+		QTECDE * PRIUNI AS SousTotal
+	FROM COMMANDE
+	INNER JOIN FOURNISSEUR
+		ON COMMANDE.NUMFOU = FOURNISSEUR.NUMFOU
+	INNER JOIN LIGNE
+		ON COMMANDE.NUMCOM = LIGNE.NUMCOM
+	INNER JOIN PRODUIT
+		ON LIGNE.CODART = PRODUIT.CODART
+	WHERE OBSCOM LIKE '%' + @vobs + '%'
+	ORDER BY COMMANDE.NUMCOM
+END
+GO
+
+EXEC prc_Lst_Commandes 'ok'
+GO
+
+---------------------
+-- 3.3
+---------------------
+DROP PROCEDURE [dbo].[prc_CA_Fournisseur]
+GO
+
+CREATE PROCEDURE [dbo].[prc_CA_Fournisseur] @vfourn INT,
+	@vannee INT,
+	@result VARCHAR(20) OUTPUT
+AS
+BEGIN
+	DECLARE @count INT
+
+	SET @count = (
+			SELECT COUNT(*)
+			FROM LIGNE l
+			INNER JOIN COMMANDE c
+				ON c.NUMCOM = l.NUMCOM
+			WHERE DATEPART(yyyy, DERLIV) = @vannee
+				AND NUMFOU = @vfourn
+			);
+
+	IF (@count = 0)
+	BEGIN
+		RETURN - 100
+	END
+	ELSE
+	BEGIN
+		SET @result = (
+				SELECT FORMAT(SUM(l.PRIUNI * l.QTECDE * 1.20), 'C', 'fr-fr')
+				FROM LIGNE l
+				INNER JOIN COMMANDE c
+					ON c.NUMCOM = l.NUMCOM
+				INNER JOIN FOURNISSEUR f
+					ON f.NUMFOU = c.NUMFOU
+				WHERE DATEPART(yyyy, DERLIV) = @vannee
+					AND f.NUMFOU = @vfourn
+				GROUP BY f.NOMFOU
+				);
+	END
+END
+GO
+
+DECLARE @Res VARCHAR(20)
+DECLARE @fourni INT
+DECLARE @annee INT
+
+SET @fourni = 1802
+SET @annee = 2017
+
+EXEC prc_CA_Fournisseur @fourni,
+	@annee,
+	@Res OUTPUT
+
+SELECT 'Le CA du Fournisseur ',
+	@fourni,
+	' pour l''année ',
+	@annee,
+	' est de ',
+	@Res
+GO
+
+---------------------
+EXECUTE sp_dropmessage 50016,
+	@lang = 'all'
+GO
+
+EXECUTE sp_addmessage @msgnum = 50016,
+	@severity = 15,
+	@msgtext = N'Supplier %d does not exist',
+	@lang = 'us_english'
+GO
+
+EXECUTE sp_addmessage @msgnum = 50016,
+	@severity = 15,
+	@msgtext = N'Fournisseur %1! inexistant',
+	@lang = 'french'
+GO
+
+RAISERROR (
+		50016,
+		-1,
+		-1,
+		7000
+		)
+GO
+
+DROP PROCEDURE [dbo].[prc_CA_Fournisseur2]
+GO
+
+CREATE PROCEDURE [dbo].[prc_CA_Fournisseur2] @vfourn INT,
+	@vannee INT,
+	@result VARCHAR(20) OUTPUT
+AS
+BEGIN
+	DECLARE @count INT
+
+	SET @count = (
+			SELECT COUNT(*)
+			FROM [dbo].[FOURNISSEUR]
+			WHERE NUMFOU = @vfourn
+			);
+
+	IF (@count = 0)
+	BEGIN
+		PRINT @vfourn
+
+		RAISERROR (
+				50016,
+				15,
+				1,
+				@vfourn
+				)
+		WITH LOG
+
+		RETURN - 100
+	END
+	ELSE
+	BEGIN
+		SET @result = (
+				SELECT FORMAT(SUM(l.PRIUNI * l.QTECDE * 1.20), 'C', 'fr-fr')
+				FROM LIGNE l
+				INNER JOIN COMMANDE c
+					ON c.NUMCOM = l.NUMCOM
+				INNER JOIN FOURNISSEUR f
+					ON f.NUMFOU = c.NUMFOU
+				WHERE DATEPART(yyyy, DERLIV) = @vannee
+					AND f.NUMFOU = @vfourn
+				GROUP BY f.NOMFOU
+				);
+	END
+END
+GO
+
+-- Connexion > Gestion > Journaux SQL Server > Actuel
+DECLARE @Res VARCHAR(20)
+DECLARE @fourni INT
+DECLARE @annee INT
+
+SET @fourni = 1802
+SET @annee = 2017
+
+EXEC prc_CA_Fournisseur2 @fourni,
+	@annee,
+	@Res OUTPUT
+
+PRINT CONCAT('Le CA du Fournisseur ',
+	@fourni,
+	' pour l''année ',
+	@annee,
+	' est de ',
+	@Res)
+GO
--- Papyrus - triggers.sql
+++ Papyrus - triggers.sql
@@ -1,39 +1,340 @@
-use [Papyrus]
-GO
-
 -- 3.2.1 - AFTER DELETE
-CREATE TRIGGER [dbo].[securityDelete]
-On [dbo].[VENDRE]
+CREATE TRIGGER [Papyrus].[dbo].[securityDelete] ON [Papyrus].[dbo].[VENDRE]
 AFTER DELETE
 AS
 BEGIN
 	DECLARE @countDelete INT;
 
-	SET @countDelete = (select COUNT(*) from deleted);
+	SET @countDelete = (
+			SELECT COUNT(*)
+			FROM deleted
+			);
 
-	if (@countDelete > 1)
-		BEGIN
-			RAISERROR ('Pas plus d''une suppression à la fois', 10, 1)
-			ROLLBACK TRAN
-END
+	IF (@countDelete > 1)
+	BEGIN
+		RAISERROR (
+				'Pas plus d''une suppression à la fois',
+				10,
+				1
+				)
+
+		ROLLBACK TRANSACTION
+	END
 END
 GO
 
-
 -- 3.2.2 - AFTER UPDATE
 CREATE TABLE [Papyrus].[dbo].[ARTICLES_A_COMMANDER] (
-	CODART varchar(4) not null,
-	QTE INT not null,
-	DATECDE datetime not null
-)
-go
+	CODART VARCHAR(4) NOT NULL,
+	QTE INT NOT NULL,
+	DATECDE DATETIME NOT NULL,
+	)
+GO
 
-CREATE TRIGGER [dbo].[securityStock]
-on [dbo].[PRODUIT]
+ALTER TABLE [Papyrus].[dbo].[ARTICLES_A_COMMANDER] ADD CONSTRAINT fk_article_acommander FOREIGN KEY (CODART
+	) REFERENCES [Papyrus].[dbo].[PRODUIT]
+GO
+
+DROP TRIGGER [dbo].[securityStock];
+GO
+
+DROP PROCEDURE [dbo].[autoCommande]
+GO
+
+CREATE PROCEDURE [dbo].[autoCommande] @countStk INT,
+	@codart VARCHAR(4)
+AS
+BEGIN
+	IF (@countStk = 0)
+	BEGIN
+		RAISERROR (
+				'Stock physique vide',
+				10,
+				1
+				)
+		WITH LOG
+
+		ROLLBACK TRANSACTION
+	END
+	ELSE
+	BEGIN
+		DECLARE @countCmde FLOAT;
+
+		SET @countCmde = (
+				SELECT STKALE - (STKPHY + SUM(QTE))
+				FROM [Papyrus].[dbo].[PRODUIT]
+				INNER JOIN ARTICLES_A_COMMANDER ON ARTICLES_A_COMMANDER.CODART = PRODUIT.CODART
+				GROUP BY PRODUIT.CODART,
+					STKALE,
+					STKPHY
+				);
+
+		IF (@countCmde > 0)
+		BEGIN
+			INSERT INTO [dbo].[ARTICLES_A_COMMANDER] (
+				[CODART],
+				[QTE],
+				[DATECDE]
+				)
+			VALUES (
+				@codart,
+				@countCmde,
+				GETDATE()
+				)
+		END
+	END
+END
+GO
+
+CREATE TRIGGER [dbo].[securityStock] ON [Papyrus].[dbo].[PRODUIT]
 AFTER UPDATE
 AS
 BEGIN
+	DECLARE produits CURSOR LOCAL FORWARD_ONLY READ_ONLY
+	FOR
+	SELECT CODART,
+		STKPHY
+	FROM deleted
+
+	DECLARE @countStk INT;
+	DECLARE @codart VARCHAR(4);
+
+	OPEN produits
+
+	FETCH NEXT
+	FROM produits
+	INTO @codart,
+		@countStk
+
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		EXEC autoCommande @countStk,
+			@codart
+
+		FETCH NEXT
+		FROM produits
+		INTO @countStk,
+			@codart
+	END
+
+	CLOSE produits
+
+	DEALLOCATE produits
 END
 GO
 
 -- 3.2.3 - AFTER UPDATE
+DROP TRIGGER [dbo].[securitySatisfaction]
+GO
+
+CREATE TRIGGER [dbo].[securitySatisfaction] ON [Papyrus].[dbo].[FOURNISSEUR]
+AFTER UPDATE
+AS
+BEGIN
+	DECLARE @oldSatisf INT;
+	DECLARE @newSatisf INT;
+
+	DECLARE fournisseurs CURSOR LOCAL FORWARD_ONLY READ_ONLY
+	FOR
+	SELECT I.SATISF,
+		D.SATISF
+	FROM inserted I
+	INNER JOIN deleted D ON I.NUMFOU = D.NUMFOU
+
+	OPEN fournisseurs
+
+	FETCH NEXT
+	FROM fournisseurs
+	INTO @newSatisf,
+		@oldSatisf
+
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		IF (
+				@oldSatisf - @newSatisf > 3
+				OR @newSatisf - @oldSatisf > 3
+				)
+		BEGIN
+			RAISERROR (
+					'Erreur de saisie de l''indice de satisfaction',
+					10,
+					1
+					);
+
+			ROLLBACK TRANSACTION
+		END
+
+		FETCH NEXT
+		FROM fournisseurs
+		INTO @newSatisf,
+			@oldSatisf
+	END
+END
+
+
+
+
+-----------------------------------------
+-------- Correction
+-----------------------------------------
+
+-- 1er trigger
+ 
+CREATE TRIGGER trg_PreventMultiDelete
+ON VENDRE
+AFTER DELETE
+AS
+BEGIN
+    -- Vérifie le nombre de lignes supprimées
+    IF (SELECT COUNT(*) FROM deleted) > 1
+	-- if @@ROWCOUNT > 1
+    BEGIN
+     -- 16 = niveau de séverité (peux etre modif par utilisateur  1 = etat (aide a la tracabilité)
+        RAISERROR ('Suppression multiple non autorisée dans la table VENTE.', 16  , 1 );
+        ROLLBACK TRANSACTION;-- annule l opération si l utilisateur essaye de supp pls lignes
+    END
+END
+ 
+-- 2ième trigger
+ 
+/* Création de la table */
+CREATE TABLE ARTICLE_A_COMMANDER(
+	CODART varchar(4) NOT NULL,
+	QTE int NOT NULL,
+	DATEALERT datetime NOT NULL,
+	FOREIGN KEY(CODART) REFERENCES PRODUIT(CODART)
+)
+GO
+/* Création du trigger */
+CREATE TRIGGER produit_modifStock ON PRODUIT
+FOR UPDATE
+AS
+BEGIN
+	-- On ne s'interesse qu'aux modifications du stock courant
+	IF 
+		UPDATE (STKPHY)
+	BEGIN
+		-- Déclaration et ouverture du curseur
+		DECLARE curseur CURSOR
+		FOR
+		SELECT CODART, STKPHY, STKALE
+		FROM inserted
+		OPEN curseur
+		-- Chargement de la première ligne
+		DECLARE @produit VARCHAR(4)
+		DECLARE @quantite INT
+		DECLARE @stockAlerte INT
+		FETCH NEXT
+		FROM curseur
+		INTO @produit, @quantite, @stockAlerte
+		-- Parcours de inserted
+		WHILE @@FETCH_STATUS = 0
+		BEGIN
+			-- Si la modification ferait passer le stock d'un article en dessous de 0 on annule la modification
+			IF (@quantite < 0)
+			BEGIN
+				RAISERROR ('Le stock de l''article est insuffisant', 16, -1, @produit) WITH LOG
+				-- Fermeture du curseur et rollback
+				CLOSE curseur
+				DEALLOCATE curseur
+				ROLLBACK TRAN
+				RETURN
+			END
+			-- Vérification de la nécessité de commander pour l'article courant
+			DECLARE @stockTotal INT
+			SET @stockTotal = @quantite
+			IF EXISTS(
+				SELECT *
+				FROM ARTICLE_A_COMMANDER
+				WHERE CODART = @produit
+			)
+			BEGIN
+				SET @stockTotal = (
+					SELECT SUM(QTE)
+					FROM ARTICLE_A_COMMANDER
+					WHERE CODART = @produit
+					) + @stockTotal
+			END
+			-- Si le stock total est inférieur ou égal au stock d'alerte on insert une ligne dans ARTICLE_A_COMMANDER
+			IF @stockTotal <= @stockAlerte
+			BEGIN
+				INSERT INTO ARTICLE_A_COMMANDER
+				VALUES (@produit, (@stockAlerte - @stockTotal), CURRENT_TIMESTAMP)
+			END
+			-- Passage à la ligne suivante
+			FETCH NEXT
+			FROM curseur
+			INTO @produit, @quantite, @stockAlerte
+		END
+		-- Fermeture du curseur
+		CLOSE curseur
+		DEALLOCATE curseur
+	END
+END
+GO
+ 
+-- v2
+CREATE TRIGGER trg_UpdateStock
+ON [dbo].[PRODUIT]
+AFTER UPDATE
+AS
+BEGIN
+    DECLARE @CODART VARCHAR(10), @STKPHY INT, @STKALE INT;
+    DECLARE StockCursor CURSOR FOR
+    SELECT i.CODART, i.STKPHY, i.STKALE
+    FROM inserted i;
+    OPEN StockCursor;
+    FETCH NEXT FROM StockCursor INTO @CODART, @STKPHY, @STKALE;
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        -- Vérifie si une réduction est effectuée lorsque le stock est déjà à 0
+        IF @STKPHY < (SELECT STKPHY FROM deleted WHERE CODART = @CODART) AND (SELECT STKPHY FROM deleted WHERE CODART = @CODART) = 0
+        BEGIN
+            RAISERROR ('Impossible de réduire le stock : le stock physique est déjà égal à 0', 16, 1) WITH LOG;
+            ROLLBACK;
+            CLOSE StockCursor;
+            DEALLOCATE StockCursor;
+            RETURN;
+        END
+        -- Insère dans ARTICLES_A_COMMANDER si le stock devient inférieur ou égal au stock d’alerte
+        IF @STKPHY + ISNULL((SELECT SUM(QTE) FROM [dbo].[ARTICLES_A_COMMANDER] WHERE CODART = @CODART), 0) <= @STKALE
+        BEGIN
+            INSERT INTO [dbo].[ARTICLES_A_COMMANDER] (CODART, QTE, DATE)
+            VALUES (@CODART, @STKALE - (@STKPHY + ISNULL((SELECT SUM(QTE) FROM [dbo].[ARTICLES_A_COMMANDER] WHERE CODART = @CODART), 0)), GETDATE());
+        END
+        FETCH NEXT FROM StockCursor INTO @CODART, @STKPHY, @STKALE;
+    END
+    CLOSE StockCursor;
+    DEALLOCATE StockCursor;
+END;
+GO
+ 
+-- trigger n°3
+CREATE TRIGGER trg_CheckSatisfactionUpdate
+ON [dbo].[FOURNISSEUR]
+AFTER UPDATE
+AS
+BEGIN
+    DECLARE @NUMFOU INT, @SATISF_NEW INT, @SATISF_OLD INT;
+    DECLARE SatisfactionCursor CURSOR FOR
+    SELECT i.NUMFOU, i.SATISF, d.SATISF
+    FROM inserted i
+    JOIN deleted d ON i.NUMFOU = d.NUMFOU;
+    OPEN SatisfactionCursor;
+    FETCH NEXT FROM SatisfactionCursor INTO @NUMFOU, @SATISF_NEW, @SATISF_OLD;
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        IF ABS(@SATISF_NEW - @SATISF_OLD) > 3
+        BEGIN
+            RAISERROR ('La modification de l indice de satisfaction est supérieure à 3 points.', 16, 1);
+            ROLLBACK;
+            CLOSE SatisfactionCursor;
+            DEALLOCATE SatisfactionCursor;
+            RETURN;
+        END
+        FETCH NEXT FROM SatisfactionCursor INTO @NUMFOU, @SATISF_NEW, @SATISF_OLD;
+    END
+    CLOSE SatisfactionCursor;
+    DEALLOCATE SatisfactionCursor;
+END;
+GO
